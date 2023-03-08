cd0955b0106db1dbd5922dd711a098d44f520888
German Alexis <alexisdager93@hotmail.com>
2023-03-08 08:53:21 -0500

    Add method logger

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { SignUpInput } from 'src/auth/dto/inputs/singup-input';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
@@ -6,6 +11,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
+  private logger: Logger = new Logger('UserServices');
   constructor(
     @InjectRepository(User)
     private readonly userReposistory: Repository<User>,
@@ -16,7 +22,7 @@ export class UsersService {
       const newUser = this.userReposistory.create(signupInput);
       return await this.userReposistory.save(newUser);
     } catch (error) {
-      throw new Error(error);
+      this.handleDBErrors(error);
     }
   }
 
@@ -31,4 +37,12 @@ export class UsersService {
   // async block(id: string): Promise<User> {
   //   return;
   // }
+
+  private handleDBErrors(error: any): never {
+    if (error.code === '23505')
+      throw new BadRequestException(error.detail.replace('key ', ''));
+
+    this.logger.error(error);
+    throw new InternalServerErrorException('Plase check server log');
+  }
 }
